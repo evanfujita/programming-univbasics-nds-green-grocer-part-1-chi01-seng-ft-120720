cc53ab528dda39300b5172e3563c5c9f69babda5
Evan Fujita <evanfujita@gmail.com>
2020-11-29 21:17:30 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,7 +5,7 @@ def find_item_by_name_in_collection(name, collection)
   collection.each do |item|
       if item[:item] == name
         cart << item   
-        binding.pry
+
     end
   end
     cart
